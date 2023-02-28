d36e0403bab7b88b70877b01c77b2d3eb67e54a8
Nakamu987 <52648066+Nakamu987@users.noreply.github.com>
2023-02-28 14:37:10 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,13 @@
 module.exports = {
 
-  publicPath: './',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/agora_webrtc_troubleshooting'
+    : '',
+
+  // If you need local debugging on the phone
+  // devServer: {
+  //   https: true
+  // }
   //publicPath: process.env.NODE_ENV === 'production'
   //  ? '/agora_webrtc_troubleshooting'
   //  : '',
